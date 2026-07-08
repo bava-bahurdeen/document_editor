b54dc012c1914b679580666d4975ab39cce1420b
bava-bahurdeen <bavabahurdeen147@gmail.com>
2026-07-08 18:23:49 +0530

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,8 @@
-import NextAuth from "next-auth";
 import { NextResponse } from "next/server";
-import { authConfig } from "./auth.config";
 
-const { auth } = NextAuth(authConfig);
-
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  const { nextUrl } = req;
-
-  const isPublicRoute = nextUrl.pathname === "/" || nextUrl.pathname === "/login";
-
-  if (nextUrl.pathname === "/login" && isLoggedIn) {
-    return NextResponse.redirect(new URL("/dashboard", nextUrl));
-  }
-
-  if (!isLoggedIn && !isPublicRoute) {
-    return NextResponse.redirect(new URL("/login", nextUrl));
-  }
-
+export function middleware() {
   return NextResponse.next();
-});
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
